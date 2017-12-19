7892c920e8003ba421c7da431399f374f1290a6d
Denis <denis@macbook-air-denis.local>
2017-12-20 02:09:36 +0300

    first IOS commit

--- Roll Dice/ViewController.swift
+++ Roll Dice/ViewController.swift
@@ -10,16 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var rightImageView: UIImageView!
+    @IBOutlet weak var leftImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        let numberOne = arc4random_uniform(6) + 1
+        
+        let numberTwo = arc4random_uniform(6) + 1
+        
+        label.text = "The sum is: \(numberOne + numberTwo)"
+        
+        leftImageView.image = UIImage(named: "Dice\(numberOne)")
+        
+        rightImageView.image = UIImage(named: "Dice\(numberTwo)")
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
